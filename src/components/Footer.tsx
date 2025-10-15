fe8fb5787aa101aa56d093a942e0f9e7d4763eca
Ethan TAYLAN <ethtaylan@gmail.com>
2025-10-15 20:48:34 +0200

    feat: MVP

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,15 @@
  * Displays GitHub link and random fact
  */
 
-import { useMemo } from "react";
+// import { useMemo } from "react";
 import { Github } from "lucide-react";
-import { facts } from "../constants";
+// import { facts } from "../constants";
 
 export const Footer: React.FC = () => {
-  const randomFact = useMemo(() => {
-    const index = Math.floor(Math.random() * facts.length);
-    return facts[index];
-  }, []);
+  // const randomFact = useMemo(() => {
+  //   const index = Math.floor(Math.random() * facts.length);
+  //   return facts[index];
+  // }, []);
 
   return (
     <div className="absolute p-4 gap-3 flex flex-col items-center bottom-0 text-sm text-gray-600">
